e2bdc342e8320025e2ece711b7847ee2d38c95fc
Haris Qazi <48071052+harisqazi1@users.noreply.github.com>
2022-06-09 03:38:59 -0500

    Update pop-os_script.sh

--- pop-os_script.sh
+++ pop-os_script.sh
@@ -1,16 +1,131 @@
 #!/bin/bash
 : '
 Author: Haris
-Name: Pop_OS! Test Script
+Name: Pop_OS! Setup Script
+Description: A script to help in downloading the essential softwares needed for my setup.
+RESOURCES:
+https://www.patorjk.com/software/taag/ -> ASCII Art (Font: Slant)
+https://support.system76.com/articles/install-in-vm/ -> Testing Code
+https://www.addictivetips.com/ubuntu-linux-tips/install-custom-themes-and-icons-in-linux/
+https://itsfoss.com/install-themes-ubuntu/
+https://www.linuxfordevices.com/tutorials/linux/change-gnome-them
+Some Inspiration from:
+https://github.com/Clepnicx/fedora-setup/blob/master/fedora-setup.sh
+https://github.com/millerii/pop_os-customize/blob/main/PopOS-install.sh
+
 '
 echo "
-  _______________________   _____ __________  ________  ______
- /_  __/ ____/ ___/_  __/  / ___// ____/ __ \/  _/ __ \/_  __/
-  / / / __/  \__ \ / /     \__ \/ /   / /_/ // // /_/ / / /   
- / / / /___ ___/ // /     ___/ / /___/ _, _// // ____/ / /    
-/_/ /_____//____//_/     /____/\____/_/ |_/___/_/     /_/     
-                                                                     
+    ____   ____   ____   __      ____  _____    _____              _         __ 
+   / __ \ / __ \ / __ \ / /     / __ \/ ___/   / ___/ _____ _____ (_)____   / /_
+  / /_/ // / / // /_/ // /     / / / /\__ \    \__ \ / ___// ___// // __ \ / __/
+ / ____// /_/ // ____//_/     / /_/ /___/ /   ___/ // /__ / /   / // /_/ // /_  
+/_/     \____//_/    (_)______\____//____/   /____/ \___//_/   /_// .___/ \__/  
+                       /_____/                                   /_/            
 "
+
+echo "Getting APT applications"
+#Gnome Tweaks
+sudo apt install gnome-tweaks -y
+#mpv video player
+sudo apt install mpv -y
+#vlc video player
+sudo apt install vlc -y
+#atom IDE
+sudo apt install atom -y
+#Backup software 
+sudo apt install deja-dup -y
+#cleaner software
+sudo apt install bleachbit -y 
+#Firefox
+sudo apt install firefox -y
+#flameshot screenshot software
+sudo apt install flameshot -y
+#nvtop install - nvidia htop monitoring tool
+sudo apt install nvtop -y
+#htop - process viewer linux
+sudo apt install htop -y
+#bashtop Linus resource monitor
+sudo apt install bashtop -y
+#Brave Browser 
+sudo apt install apt-transport-https -y
+sudo apt install curl -y
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"| sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+apt update
+sudo apt install brave-browser -y
+#KeepassXC password manager
+sudo apt install keepassxc -y
+#kid3 mp3 tag editor
+sudo apt install kid3 -y
+#comic book reader
+sudo apt install mcomix -y
+#OBS Studio
+sudo apt install obs-studio -y
+#qbittorrent - torrent downloader
+sudo apt install qbittorrent -y
+#RhythmBox Audio Player
+sudo apt install rhythmbox -y
+#Thunderbird Mail
+sudo apt install thunderbird -y
+#Variety Wallpaper Changer
+sudo apt install variety -y
+#Steam
+sudo apt install steam -y
+#VirtualBox
+sudo apt install virtualbox -y 
+#neofetch install
+sudo apt install neofetch -y
+
+
+echo "Flatpak installation"
+echo "---------------------"
+sudo apt install flatpak
+flatpak remote-add  --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+cd Flatpack_files #download the files here for easy clean up
+# -y assumes yes for the questions
+echo "Citra Gaming Emulator"
+flatpak install flathub org.citra_emu.citra -y 
+echo "Installing Bitwarden"
+flatpak install flathub com.bitwarden.desktop -y
+echo "Installing Discord"
+flatpak install flathub flathub com.discordapp.Discord -y
+echo "Installing GitHub Desktop"
+flatpak install flathub io.github.shiftey.Desktop -y
+echo "Installing gPodder"
+flatpak install flathub org.gpodder.gpodder -y
+echo "Installing Heroic Games Launcher"
+flatpak install flathub com.heroicgameslauncher.hgl -y
+echo "Installing FreeTube"
+flatpak install flathub io.freetubeapp.FreeTube -y
+echo "Installing Kdenlive"
+flatpak install flathub org.kde.kdenlive -y
+echo "Installing Librewolf"
+flatpak install flathub io.gitlab.librewolf-community -y
+echo "Installing Obsidian Markdown editor"
+flatpak install flathub md.obsidian.Obsidian -y
+echo "Installing Planner"
+flatpak install flathub com.github.alainm23.planner -y
+echo "Installing Visual Studio Code"
+flatpak install flathub com.visualstudio.code -y
+echo "Installing Vocal"
+flatpak install flathub com.github.needleandthread.vocal -y
+echo "Installing Zettlr"
+flatpak install flathub com.zettlr.Zettlr -y
+
+#non APT and Flatpak downloads
+echo "Installing ProtonVPN"
+wget https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb
+sudo dpkg -i protonvpn-stable-release_1.0.1-1_all.deb #Change link when new one comes out
+apt-get update -y
+apt-get install protonvpn -y
+
+echo "Installing Signal"
+# Link --> https://www.signal.org/download/linux/
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+sudo apt update && sudo apt install signal-desktop
 
 #Candy Icon Set-up --> https://github.com/EliverLara/candy-icons
 ## Gnome Look -->  https://www.opendesktop.org/p/1305251/
@@ -18,6 +133,7 @@ echo "Installing the Candy Icons"
 wget https://github.com/EliverLara/candy-icons/archive/refs/heads/master.zip
 sudo unzip master.zip -d /usr/share/icons/ #system-wide install; ~/.icons for user (I think)
 gsettings set org.gnome.desktop.interface icon-theme candy-icons-master
+
 
 #Flat Remix Theme Set-Up 
 ## https://extensions.gnome.org/extension/19/user-themes/ --> http://drasite.com/flat-remix-gnome
@@ -32,3 +148,13 @@ gsettings set org.gnome.desktop.interface gtk-theme Flat-Remix-Blue-Dark
 gsettings set org.gnome.desktop.wm.preferences theme Flat-Remix-Blue-Dark
 '
 
+echo "
+Post Install TO DO:
+https://github.com/arkenfox/user.js setup for FireFox
+Log In to all accounts
+Install Gnome extensions --> https://extensions.gnome.org/
+> Freon --> https://extensions.gnome.org/extension/841/freon/
+> Sound Input & Output Device Chooser --> https://extensions.gnome.org/extension/906/sound-output-device-chooser/
+> User Themes --> https://extensions.gnome.org/extension/19/user-themes/ --> http://drasite.com/flat-remix-gnome
+Install OhMyZSH with PowerLevel10k
+"
