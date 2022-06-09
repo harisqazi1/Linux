f4affcd9b0fd6f60cc857819501bea172f9566ab
Haris Qazi <48071052+harisqazi1@users.noreply.github.com>
2022-06-09 17:41:10 -0500

    Update pop-os_script_Icon_Theme.sh

--- pop-os_script_Icon_Theme.sh
+++ pop-os_script_Icon_Theme.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 echo "
-    ____   ____   ____   __      ____  _____    _____              _         __ 
-   / __ \ / __ \ / __ \ / /     / __ \/ ___/   / ___/ _____ _____ (_)____   / /_
-  / /_/ // / / // /_/ // /     / / / /\__ \    \__ \ / ___// ___// // __ \ / __/
- / ____// /_/ // ____//_/     / /_/ /___/ /   ___/ // /__ / /   / // /_/ // /_  
-/_/     \____//_/    (_)______\____//____/   /____/ \___//_/   /_// .___/ \__/  
-                       /_____/                                   /_/            
+   ____                ____      ________                
+  /  _/______  ___    / __/___  /_  __/ /  ___ __ _  ___ 
+ _/ // __/ _ \/ _ \   > _/_ _/   / / / _ \/ -_)  ' \/ -_)
+/___/\__/\___/_//_/  |_____/    /_/ /_//_/\__/_/_/_/\__/ 
 "
+
+echo "Making dirs for icons and themes"
+mkdir ~/.icons/
+mkdir ~/.themes/
 
 #Candy Icon Set-up --> https://github.com/EliverLara/candy-icons
 ## Gnome Look -->  https://www.opendesktop.org/p/1305251/
 echo "Installing the Candy Icons"
 wget https://github.com/EliverLara/candy-icons/archive/refs/heads/master.zip
-unzip master.zip -d /usr/share/icons #system-wide install; ~/.icons for user (I think)
+unzip master.zip -d ~/.icons/ #/usr/share/icons for system-wide install (NEED sudo)
 gsettings set org.gnome.desktop.interface icon-theme candy-icons-master
