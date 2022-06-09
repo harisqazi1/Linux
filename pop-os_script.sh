9ceda1f880cf3c5e1f4213ce9bbc574dd48eab9f
Haris Qazi <48071052+harisqazi1@users.noreply.github.com>
2022-06-09 01:25:37 -0500

    Update pop-os_script.sh

--- pop-os_script.sh
+++ pop-os_script.sh
@@ -13,12 +13,6 @@ echo "
                        /_____/                                   /_/            
 "
 
-#Make sure user is not root
-if [ "$EUID" -e 0 ]
-  then echo "Do Not run as root. Sudo included already."
-  exit
-fi
-
 echo "Getting APT applications"
 #Gnome Tweaks
 sudo apt install gnome-tweaks -y
@@ -119,18 +113,10 @@ apt-get install protonvpn -y
 
 echo "Installing Signal"
 # Link --> https://www.signal.org/download/linux/
-# NOTE: These instructions only work for 64 bit Debian-based
-# Linux distributions such as Ubuntu, Mint etc.
-
-# 1. Install our official public software signing key
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
 cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
-
-# 2. Add our repository to your list of repositories
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
   sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-
-# 3. Update your package database and install signal
 sudo apt update && sudo apt install signal-desktop
 
 #Candy Icon Set-up --> https://github.com/EliverLara/candy-icons
@@ -155,7 +141,7 @@ gsettings set org.gnome.desktop.wm.preferences theme Flat-Remix-Blue-Dark
 
 echo "
 Post Install TO DO:
-Remove folder from {user}/Desktop/
+Remove ~/Desktop/Flat
 https://github.com/arkenfox/user.js setup for FireFox
 Log In to all accounts
 Install Gnome extensions --> https://extensions.gnome.org/
